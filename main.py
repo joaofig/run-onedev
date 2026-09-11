1522cf141ca76997954ec6e80b0adc0feb70cf52
joaofig <joao.figueira@gmail.com>
2026-09-11 12:57:39 +0100

    Use source command to execute server.sh for proper environment inheritance

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import subprocess
 
 def main():
     subprocess.run(["chmod", "+x", "./bin/server.sh"])
-    subprocess.run(["./bin/server.sh", "console"])
+    subprocess.run(["source", "./bin/server.sh", "console"])
 
 
 if __name__ == "__main__":
