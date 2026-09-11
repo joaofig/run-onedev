6d96bc6b8f4e2de716a5050900ae294b0d551cf2
joaofig <joao.figueira@gmail.com>
2026-09-11 13:18:15 +0100

    Fix server script path in main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import subprocess
 
 
 def main():
-    subprocess.run(["cd", "./app/onedev-latest"])
+    subprocess.run(["chdir", "onedev-latest"])
     subprocess.run(["pwd"])
     subprocess.run(["ls", "-al"])
     subprocess.run(["./bin/server.sh", "console"])
