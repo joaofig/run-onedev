fd449b327d6702d38a2c2bc83a65bd1477b472d9
joaofig <joao.figueira@gmail.com>
2026-09-11 13:08:56 +0100

    Fix server script path in main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import subprocess
 
 
 def main():
-    subprocess.call(["chmod", "+x", "./bin/server.sh"])
-    subprocess.call(["sh", "./bin/server.sh", "console"])
+    subprocess.call(["chmod", "+x", "./onedev-latest/bin/server.sh"])
+    subprocess.call(["sh", "./onedev-latest/bin/server.sh", "console"])
 
 
 if __name__ == "__main__":
