5b2d2919cc408fd4acc14299d009d04b591ff13f
joaofig <joao.figueira@gmail.com>
2026-09-11 13:11:02 +0100

    Fix server script execution in main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import subprocess
 
 
 def main():
-    subprocess.call(["chmod", "+x", "./onedev-latest/bin/server.sh"])
-    subprocess.call(["sh", "./onedev-latest/bin/server.sh", "console"])
+    subprocess.run(["pwd"])
+    subprocess.run(["./bin/server.sh", "console"])
 
 
 if __name__ == "__main__":
