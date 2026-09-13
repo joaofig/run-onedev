2d3d13d434c9cee7bfd88d4b83394b53792638d5
joaofig <joao.figueira@gmail.com>
2026-09-13 15:23:08 +0100

    Update start.sh to use absolute path for server.sh execution

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-cd onedev-latest || exit
-chmod +x ./bin/server.sh
-source ./bin/server.sh console
+#cd onedev-latest || exit
+chmod +x ./onedev-latest/bin/server.sh
+source ./onedev-latest/bin/server.sh console
