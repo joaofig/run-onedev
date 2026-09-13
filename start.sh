5a2fc2b7567021e134b8c0fbab1ca2844a10c798
joaofig <joao.figueira@gmail.com>
2026-09-13 11:29:30 +0100

    Update startup script to source environment and server script properly

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
+source "$HOME"/.local/bin/env
+
 uv sync --no-dev --no-sources
 uv run python main.py
+
 cd onedev-latest || exit
 chmod +x ./bin/server.sh
-sh ./bin/server.sh console
+source ./bin/server.sh console
