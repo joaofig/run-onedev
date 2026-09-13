6f68c78c219f2bb12412ca2c09aefe84e64a02ac
joaofig <joao.figueira@gmail.com>
2026-09-13 12:25:15 +0100

    Install util-linux package in start.sh script

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source "$HOME"/.local/bin/env
 
-#apt install util-linux
+apt install util-linux
 
 uv sync --no-dev --no-sources
 uv run python main.py
