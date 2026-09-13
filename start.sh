fa5a67b819a4f2562959c3af8cc7b6a4765f6cbe
joaofig <joao.figueira@gmail.com>
2026-09-13 15:09:10 +0100

    Remove Python and Docker related files and dependencies

--- start.sh
+++ start.sh
@@ -1,11 +1,3 @@
-curl -LsSf https://astral.sh/uv/install.sh | sh
-source "$HOME"/.local/bin/env
-
-apt install openjdk-17-jre-headless
-
-uv sync --no-dev --no-sources
-uv run python main.py
-
 cd onedev-latest || exit
 chmod +x ./bin/server.sh
 source ./bin/server.sh console
