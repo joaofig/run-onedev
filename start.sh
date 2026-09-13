0adf47c56fbdc1c3d3e8e871323937ab68ee7e33
joaofig <joao.figueira@gmail.com>
2026-09-13 12:53:34 +0100

    Remove sudo from apt install command in start.sh script

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source "$HOME"/.local/bin/env
 
-sudo apt install openjdk-17-jre-headless
+apt install openjdk-17-jre-headless
 
 uv sync --no-dev --no-sources
 uv run python main.py
