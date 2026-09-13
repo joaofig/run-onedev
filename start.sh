54fdce117141be806d6b1740b26da344ab446b72
joaofig <joao.figueira@gmail.com>
2026-09-13 12:33:28 +0100

    Switch to absolute sudo path in start.sh script

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source "$HOME"/.local/bin/env
 
-sudo apt install util-linux
+/usr/bin/sudo apt install util-linux
 
 uv sync --no-dev --no-sources
 uv run python main.py
