718d0e83e50c0b13e1c53ed94424e43a64a36125
João Paulo Figueira <joao.figueira@gmail.com>
2026-09-15 16:39:42 +0100

    Comment out database configuration in the start script to disable overwriting with environment variables.

--- start.sh
+++ start.sh
@@ -2,11 +2,11 @@ cd onedev-latest || exit
 chmod +x bin/server.sh
 sed -i "s/http_port=6610/http_port=$PORT/" conf/server.properties
 
-sed -i "s/hibernate.connection.url=jdbc:postgresql:\/\/localhost:5432\/onedev/hibernate.connection.url=$hibernate_connection_url/" \
-  conf/hibernate.properties
-sed -i "s/hibernate.connection.username=postgres/hibernate.connection.username=$hibernate_connection_username/" \
-  conf/hibernate.properties
-sed -i "s/hibernate.connection.password=postgres/hibernate.connection.password=$hibernate_connection_password/" \
-  conf/hibernate.properties
+#sed -i "s/hibernate.connection.url=jdbc:postgresql:\/\/localhost:5432\/onedev/hibernate.connection.url=$hibernate_connection_url/" \
+#  conf/hibernate.properties
+#sed -i "s/hibernate.connection.username=postgres/hibernate.connection.username=$hibernate_connection_username/" \
+#  conf/hibernate.properties
+#sed -i "s/hibernate.connection.password=postgres/hibernate.connection.password=$hibernate_connection_password/" \
+#  conf/hibernate.properties
 
 ./bin/server.sh console
