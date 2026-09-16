69946451ff213568047fbaa71a072cffcb6a5fae
João Paulo Figueira <joao.figueira@gmail.com>
2026-09-16 09:49:33 +0100

    Remove OneDev server package download and extraction from the start script.

--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
-wget https://code.onedev.io/onedev/server/~site/onedev-latest.tar.gz
-tar -xvzf onedev-latest.tar.gz
-
 cd onedev-latest || exit
 chmod +x bin/server.sh
 sed -i "s/http_port=6610/http_port=$PORT/" conf/server.properties
